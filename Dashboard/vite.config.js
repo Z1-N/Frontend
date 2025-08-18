0c487d9fe7a0ff62444f4050f28c966b45d8df55
Z1-N <alltayeb112003@gmail.com>
2025-08-18 22:10:11 +0400

    deploy to github pages

--- Dashboard/vite.config.js
+++ Dashboard/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// If deploying to GitHub Pages, set BASE to '/<repo>/'
+// You can override via VITE_BASE at build time
+const BASE = process.env.VITE_BASE || '/';
+
 export default defineConfig({
+  base: BASE,
   plugins: [react(), tailwindcss()],
 })
